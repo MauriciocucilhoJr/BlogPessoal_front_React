d63948c9b6d7d6b2b7808e2d30171851c56b9127
Mauriciocucilhojr <mauriciocucilhojunior@gmail.com>
2022-10-05 15:47:06 -0300

    React Generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Componets/Estaticos/Navbar/Navbar';
+import Footer from './Componets/Estaticos/Footer/Footer';
+import Home from './Paginas/Home/Home';
 import './App.css';
+import Login from './Paginas/Login/Login';
+import CadastroUsuario from './Paginas/cadastroUsuario/CadastroUsuario';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+      </Routes>
+      <Footer />
+    </Router>
+
   );
 }
 
